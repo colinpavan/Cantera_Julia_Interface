3581849e54dee6e953d29aae9e424289c4ba1ac3
Colin Pavan <cpavan@mit.edu>
2021-10-20 12:11:46 -0400

    updated demo

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# PAC_Flame_Model
+# Cantera_Julia_Interface
+# cpavan 2021-10-20
 
-Testing github functionality
+This project is a julia interface for Cantera
+I have been building it and adding functionality as I need it, but it is far from done
+Currently, it is just a way to access the property data
 
-testing pull
+To use, the library libcantera_shared.so must be on your system search path (on linux)
+this library can be constructed by directly compiling the cantera c++ source code
+follow instructions on: https://cantera.org/
+
+Some examples of usage are in the file cantera_interface_demo
--- cantera_interface_demo.jl
+++ cantera_interface_demo.jl
@@ -4,7 +4,7 @@ Pkg.activate(".")
 # to use this, the library libcantera_shared.so must be on your system search path
 ct=include("./cantera_interface/cantera_interface.jl")
 
-# gas array with direct cantera interface
+# gas with direct cantera interface
 gas1=ct.gas("gri30.yaml")
 # same as above with initialization of TPX
 gas2=ct.gas("gri30.yaml",(300.0,cantera.one_atm,"CH4:1,O2:2,N2:7.52"))
@@ -19,3 +19,17 @@ ct.update_gas(gas2_loc)
 # now the local copy is updated
 gas2_loc.T
 gas2_loc.X
+
+# gas array - usefull for 1D simulations
+Nel=50
+gas_array=ct.solutionArray("gri30.yaml",Nel,(300.0,cantera.one_atm,"CH4:1,O2:2,N2:7.52"))
+# local copy for fast access
+gas_array_local=ct.solutionArray_local(gas_array)
+[ct.set_T(gas_array.phase[i], 300.0+i) for i=1:Nel]
+ct.update_gas_array(gas_array_local)
+# compare timing of local vs library copies - 2 order of magnitude difference!
+using BenchmarkTools
+@btime gas_array_local.T
+@btime ct.get_T(gas_array)
+# if you are going to access a property more than once
+# it will always be more efficient to store it in the local object
